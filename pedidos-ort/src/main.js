d7d9a2381a084424aed546c81ff5629137eff2c9
NahuelLizzi <nahulizzi.facu@gmail.com>
2025-09-16 21:56:15 -0300

    Inicio Header

--- pedidos-ort/src/main.js
+++ pedidos-ort/src/main.js
@@ -1,5 +1,26 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import {createRouter, createWebHistory} from 'vue-router';
 
-createApp(App).mount('#app')
+
+//Definir las rutas
+const routes = [{
+    path: '/',
+    component: Home
+}];
+
+//Crear una instacia del router y pasarle las rutas
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+// Hooks para ejecutar antes de ir hacia cada ruta.
+// router.beforeEach((to,from,next)=>{
+
+// })
+
+const app = createApp({});
+app.use(router);
+App.mount('#app');
