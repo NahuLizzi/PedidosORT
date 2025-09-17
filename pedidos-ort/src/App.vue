75186702815a17131737745d22de449468c8c356
NahuelLizzi <nahulizzi.facu@gmail.com>
2025-09-16 22:30:38 -0300

    v1.0.0 v-bind link

--- pedidos-ort/src/App.vue
+++ pedidos-ort/src/App.vue
@@ -1,13 +1,24 @@
 <script setup>
-import {useRoute} from 'vue-router';
+import {RouterLink, RouterView, useRoute} from 'vue-router';
+import {ref, computed} from 'vue'
+
+const link = ref("/")
+const linksVisualizados = computed({
+  
+})
+
 </script>
+
+
 
 <template>
   <header class="">
+    <RouterLink :to='link' >Home</RouterLink>
     <div class="">
-
+      
     </div>
   </header>
+  <RouterView></RouterView>
   
 </template>
 
